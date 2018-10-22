4a7b5e2418931d8cbd86f4e5bf7e80dbc3bbd7b8
Yoenas <yoenas@yoenas.local>
2018-10-22 14:12:20 +0700

    Initial Commit

---
+++ Mapkit iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mapkit iOS
+//
+//  Created by Yoenas on 22/10/18.
+//  Copyright © 2018 Yoenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
